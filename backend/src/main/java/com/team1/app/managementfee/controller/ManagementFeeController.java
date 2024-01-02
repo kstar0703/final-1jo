2810b3cbd315fbea54d991bd2f024ccb5e972da1
DESKTOP-RO4OUHM\user1 <corey5361@nate.com>
2024-01-02 15:48:54 +0900

    feat: URL 설계에 따른 기능별 Controller 생성

---
+++ backend/src/main/java/com/team1/app/managementfee/controller/ManagementFeeController.java
@@ -0,0 +1,6 @@
+package com.team1.app.managementfee.controller;
+
+//°ü¸®ºñ
+public class ManagementFeeController {
+
+}
---
+++ backend/src/main/java/com/team1/app/parking/controller/ParkingController.java
@@ -0,0 +1,6 @@
+package com.team1.app.parking.controller;
+
+//방문 예약
+public class ParkingController {
+
+}
