826ba510dc7c669a293fd980d819949f2296496f
DESKTOP-RO4OUHM\user1 <corey5361@nate.com>
2024-01-05 17:51:05 +0900

    feat: Mapper.xml, Vo 생성, config.xml VO 별칭 수정

---
+++ backend/src/main/java/com/team1/app/complaint/vo/ComplaintVo.java
@@ -0,0 +1,24 @@
+package com.team1.app.complaint.vo;
+
+import lombok.Data;
+
+@Data
+public class ComplaintVo {
+                                                            
+	private String complaintNo;		//민원 번호                 
+	private String managerNo;		//민원 처리 담당자 관리자 번호
+	private String memberNo;		//작성자 번호                
+	private String memberName;		//작성자 이름                
+	private String title;			//민원 제목                 
+	private String content;			//민원 내용                 
+	private String enrollDate;		//작성 일자                 
+	private String delYn;			//삭제 여부                 
+	private String status;			//민원 처리 상태              
+	private String reply;			//민원 처리 답변              
+	private String replyDate;		//민원 처리 일자              
+	                                                        
+	private String imgNo;			//이미지 번호                
+	private String imgName;			//이미지 이름                
+	private String path;			//이미지 경로
+	private String originName;		//이미지 원래 이름             
+}                                                           
