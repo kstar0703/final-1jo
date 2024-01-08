a9c32de56ea1201f49da212875038bfe0edd7b21
DESKTOP-RO4OUHM\user1 <corey5361@nate.com>
2024-01-08 17:49:14 +0900

    feat : mapper.xml 구현 기능 추가, vo 수정

--- backend/src/main/java/com/team1/app/complaint/vo/ComplaintVo.java
+++ backend/src/main/java/com/team1/app/complaint/vo/ComplaintVo.java
@@ -11,11 +11,15 @@ public class ComplaintVo {
 	private String memberName;		//작성자 이름                
 	private String title;			//민원 제목                 
 	private String content;			//민원 내용                 
-	private String enrollDate;		//작성 일자                 
+	private String enrollDate;		//작성 일자
+	private String enrollDateStart;	//작성 일자 검색 시작 일자 
+	private String enrollDateEnd;	//작성 일자 검색 종료 일자
 	private String delYn;			//삭제 여부                 
 	private String status;			//민원 처리 상태              
 	private String reply;			//민원 처리 답변              
-	private String replyDate;		//민원 처리 일자              
+	private String replyDate;		//민원 처리 일자
+	private String replyDateStart;	//민원 처리 일자 검색 시작 일자 
+	private String replyDateEnd;	//민원 처리 일자 검색 종료 일자
 	                                                        
 	private String imgNo;			//이미지 번호                
 	private String imgName;			//이미지 이름                
