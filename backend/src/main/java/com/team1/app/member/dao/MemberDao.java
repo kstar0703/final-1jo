3ba671fa36feb44630a5221d2c8346ba37b3e6be
kstar0703 <kstar966@gmail.com>
2024-01-08 18:30:07 +0900

    feat : 로그인 기능 추가
    로그인 기능 추가 및 service 로그인 로직추가

--- backend/src/main/java/com/team1/app/member/dao/MemberDao.java
+++ backend/src/main/java/com/team1/app/member/dao/MemberDao.java
@@ -8,9 +8,16 @@ import com.team1.app.member.vo.MemberVo;
 @Repository
 public class MemberDao {
 
+	
+	// 아이디 유효성 확인
 	public int validateId(MemberVo vo, SqlSessionTemplate sst) {
 		
 		return sst.selectOne("MemberMapper.validateId",vo);
+	}
+
+	//회원가입
+	public int join(SqlSessionTemplate sst, MemberVo vo) {
+		return 0;
 	}
 	
 	
--- backend/src/main/java/com/team1/app/member/util/MemberUtil.java
+++ backend/src/main/java/com/team1/app/member/util/MemberUtil.java
@@ -9,18 +9,34 @@ import org.springframework.stereotype.Component;
 public class MemberUtil {
 
 	//전화 번호 형식 확인
-	public boolean matchParttern(String phoneNumber) {
+	public boolean matchPhoneParttern(String phoneNumber) {
 		  final String PHONE_NUMBER_REGEX = "^010-\\d{4}-\\d{3}$";
 		 Pattern pattern = Pattern.compile(PHONE_NUMBER_REGEX);
 	        Matcher matcher = pattern.matcher(phoneNumber);
 	        return matcher.matches();
 	}
 
-	//전화 번호 길이 확인
-	public boolean checkLength(String phoneNumber) {		
-		return phoneNumber.length() <= 12;
+	//전화 번호 길이 확인 
+	public boolean checkPhoneLength(String phoneNumber) {		
+		return phoneNumber.length() <= 12 || phoneNumber.length()>13;
 	}
 	
+	//패스워드 길이 확인 (9자리 이상)
+	public boolean checkIdLength(String pwd) {
+		
+		return pwd.length() <=8;	
+			}
+	//패스워드 형식
+	public boolean matchIdParttern(String pwd) {
+		   // 정규식 패턴
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$";
+        // 패턴 컴파일
+        Pattern pattern = Pattern.compile(regex);
+        // 패턴과 일치하는지 확인
+        Matcher matcher = pattern.matcher(pwd);
+
+        return matcher.matches();
+	}
 	
 
 }
