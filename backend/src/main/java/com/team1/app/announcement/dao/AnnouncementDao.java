b465ca9bab5ed2bff6369e8fb1f0f420729ac6d3
kstar0703 <kstar966@gmail.com>
2024-01-10 17:50:48 +0900

    feat : 게시글 작성 ,전체조회 구현
    게시글 상세조회 확인중...

--- backend/src/main/java/com/team1/app/announcement/dao/AnnouncementDao.java
+++ backend/src/main/java/com/team1/app/announcement/dao/AnnouncementDao.java
@@ -2,25 +2,30 @@ package com.team1.app.announcement.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.team1.app.announcement.vo.AnnouncementVo;
+import com.team1.app.util.vo.PageVo;
 
 @Repository
 public class AnnouncementDao {
 
 	//공지사항 작성(admin)이미지 첨부
 	public int write(SqlSessionTemplate sst, AnnouncementVo vo) {
+		
+		
 		return sst.insert("Announcement.write",vo);
 	}
-	//공지사항 게시판 글개수 (페이징용)
-	public int count(SqlSessionTemplate sst, AnnouncementVo vo) {
-		return sst.selectOne("Announcement.count",vo);
-	}
-	//공지사항 목록 조회 
-	public List<AnnouncementVo> list(SqlSessionTemplate sst, AnnouncementVo vo) {
-		return null;
+	//공지사항 목록 조회
+	public List<AnnouncementVo> list(SqlSessionTemplate sst, AnnouncementVo vo,PageVo pageVo) {
+		
+		int limit = pageVo.getBoardLimit();
+		int offset = (pageVo.getCurrentPage()-1) * pageVo.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset,limit);
+		
+		return sst.selectList("Announcement.list",vo,rowBounds);
 	}
 	//공지사항 상세조회
 	public List<AnnouncementVo> detail(SqlSessionTemplate sst, AnnouncementVo vo) {
