af7eb47c1834ca1208b15c3d2c3ec8e34d645f7a
kstar0703 <kstar966@gmail.com>
2024-01-11 16:18:27 +0900

    Feat PageVo 버그 수정
    fix PageVo this 누락으로 버그생긴거 수정했습니다

--- backend/src/main/java/com/team1/app/util/vo/PageVo.java
+++ backend/src/main/java/com/team1/app/util/vo/PageVo.java
@@ -1,8 +1,14 @@
 package com.team1.app.util.vo;
 
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+
+@NoArgsConstructor
+@ToString
 @Getter
 public class PageVo {
 	
@@ -14,12 +20,29 @@ public class PageVo {
 	private int maxPage;		// 가장 마지막 페이지 
 	private int startPage;		// 페이징 영역 시작값 
 	private int endPage; 		// 페이징 영역 마지막값
+	public int getCurrentPage() {
+		return currentPage;
+	}
 	
-/*	private int startRow; 		// 조회할 첫번재 행 번호 (ROWNUM)
-	private int lastRow; 		// 조회할 마지막 행 번호 (ROWNUM)
-*/	
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+	
+	public void setBoardLimit(int boardLimit) {
+		this.boardLimit = boardLimit;
+	}
+	
+	
+	
+	
+
+	//private int startRow; 		// 조회할 첫번재 행 번호 (ROWNUM)
+	//private int lastRow; 		// 조회할 마지막 행 번호 (ROWNUM)
+	
 	public PageVo(int listCount , int currentPage, int pageLimit, int boardLimit) {
 		this.listCount = listCount;
+		
+		System.out.println("들어온 현재 페이지" +currentPage);
 		
 		if(currentPage<=0) {
 			this.currentPage = 1;
@@ -29,40 +52,43 @@ public class PageVo {
 		
 		this.pageLimit = pageLimit;
 		
+		
 		if(boardLimit<=0) {
 			this.boardLimit =10;
 		}else {
 			this.boardLimit = boardLimit;
 		}
 		
-		this.maxPage = (int) Math.ceil((double)listCount/boardLimit);
-		this.startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
-		this.endPage = startPage + pageLimit - 1;
+		System.out.println("");
+		
+		System.out.println(boardLimit);
+		System.out.println((double)listCount/boardLimit ) ;
+		
+		this.maxPage = (int) Math.ceil((double)this.listCount/this.boardLimit);
+		this.startPage = (this.currentPage - 1) / this.pageLimit * this.pageLimit + 1;
+		this.endPage = this.startPage + this.pageLimit - 1;
 		
 		if(endPage > maxPage) {
 			endPage = maxPage;
 		}
 		
-		/*
-		 * this.startRow = (currentPage - 1) * boardLimit + 1; this.lastRow = startRow +
-		 * boardLimit - 1;
-		 */
 	}
-
-	public void setCurrentPage(int currentPage) {
-		this.currentPage = currentPage;
-	}
-	public void setBoardLimit(int boardLimit) {
-		this.boardLimit = boardLimit;
-	}
-	
-	
-
-	
-	
-	
-	
 }
+		
+		
+		
+
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
 
 	
 	
