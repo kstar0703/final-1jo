b3d63e06c116d03b26b2e7215483d63860a530e3
DESKTOP-GREBV9H\user1 <230420mkp@gmail.com>
2024-01-12 17:59:37 +0900

    feat: Modify code - Facility

--- frontend/src/components/facility/FacilityDetail.jsx
+++ frontend/src/components/facility/FacilityDetail.jsx
@@ -1,10 +1,65 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import FacilityNoticeList from './notice/FacilityNoticeList';
+import FacilityHistoryWrite from './history/FacilityHistoryWrite';
+import FacilityHistoryWriteWithClick from './history/FacilityHistoryWriteWithClick';
 
 const FacilityDetail = () => {
+    let facilitiesNo = useParams();
+
     return (
         <div>
-            커뮤니티 목록 상세
-            
+            <div>
+                커뮤니티 목록 상세<br/>
+                --------------------------------------
+                <div>사우나</div>
+                <div>사우나사진</div>
+                    --------------------------------------
+                <div>
+                    <div>[ 시설정보 ]</div>
+                    <div>운영안내</div>
+                    <table>
+                        <tr>
+                            <th>시설 위치</th>
+                            <td>커뮤니티 센터</td>
+                        </tr>
+                        <tr>
+                            <th>운영 시간</th>
+                            <td>월~금</td>
+                        </tr>
+                        <tr>
+                            <th>휴일</th>
+                            <td>월 휴관</td>
+                        </tr>
+                        <tr>
+                            <th>문의</th>
+                            <td>02-222-2222</td>
+                        </tr>
+                        <tr>
+                            <th>편의시설</th>
+                            <td>정수기</td>
+                        </tr>
+                        <tr>
+                            <th>안내사항</th>
+                            <td>목적외 사용불가</td>
+                        </tr>
+                    </table>
+                    --------------------------------------
+                </div>
+                <div>
+                    <div>[ 공지사항 ]</div>
+                    <FacilityNoticeList />
+                    --------------------------------------
+                </div>
+                <div>
+                    <div>[ 예약신청 ]</div>
+                    <FacilityHistoryWrite />
+                    선택2
+                    <FacilityHistoryWriteWithClick />
+                    --------------------------------------
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 };
--- frontend/src/components/facility/FacilityMain.jsx
+++ frontend/src/components/facility/FacilityMain.jsx
@@ -1,38 +1,18 @@
-import React, { useState } from 'react';
-import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import FacilityHistoryList from './history/FacilityHistoryList';
+import FacilityList from './FacilityLayout';
 import FacilityDetail from './FacilityDetail';
-import FacilityDetailItem from './FacilityDetailItem';
+import FacilityHistoryComplete from './history/FacilityHistoryComplete';
 
 const FacilityMain = () => {
-    const facilitiesNo = useParams();
-    const navigator = useNavigate();
-    const [facilityVoList, setFacilityVoList] = useState([]);
-    const loadFacilityVoList = ()=>{
-        fetch("http://127.0.0.1:8888/app/facility/list")
-        .then(resp=>resp.json())
-        .then(data=>{setFacilityVoList(data.facilityVoList);});
-    }
-    loadFacilityVoList();
-    return (
-        <div>
-            <div>My 예약 내역</div>
-            <div>
-                    {
-                        facilityVoList.length === 0?
-                        <h1>loading..</h1>
-                        :
-                        facilityVoList.map(vo=>
-                            <div onClick={navigator(`detail/${vo.facilitiesNo}`)}>
-                                <FacilityDetailItem img={vo.image} fName={vo.facilitiesName}/>
-                            </div>
-                        )
-                    }
 
-                <Routes>
-                    <Route path='detail/:facilitiesNo' element={<FacilityDetail />}/>
-                </Routes>
-            </div>
-        </div>
+    return (
+        <Routes>
+            <Route path='history' element={<FacilityHistoryList />}/>
+            <Route path='list' element={<FacilityList />}/>
+            <Route path='detail/:facilitiesNo' element={<FacilityDetail />}/>
+            <Route path='complete' element={<FacilityHistoryComplete />}/>
+        </Routes>
     );
 };
 
