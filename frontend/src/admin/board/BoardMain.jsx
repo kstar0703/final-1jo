0b65e08c518357a128ee688d4a1ed4096fa45827
DESKTOP-IO4SNS9\mhhh <230420mkp@gmail.com>
2024-01-17 17:52:18 +0900

    feat: Modify code to change admin route - Board

--- frontend/src/admin/board/BoardMain.jsx
+++ frontend/src/admin/board/BoardMain.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import BoardList from './BoardList';
 
-
 const BoardMain = () => {
     return (
         <Routes>
-            <Route path='admin/list' element={<BoardList />}/>    
+            <Route path='list' element={<BoardList />}/>    
             admin  
         </Routes>
     );
