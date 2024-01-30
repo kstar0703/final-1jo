de22fe1190e866993bc9709cbdb6c7369de349e7
DESKTOP-IO4SNS9\mhhh <230420mkp@gmail.com>
2024-01-30 09:07:31 +0900

    feat: Modifying code to insert usage of community faculty - ManagementFee

--- backend/src/main/java/com/team1/app/managementfee/dao/ManagementFeeDao.java
+++ backend/src/main/java/com/team1/app/managementfee/dao/ManagementFeeDao.java
@@ -12,7 +12,7 @@ public class ManagementFeeDao {
 
 	//세대별 관리비 조회(세대정보, 기간)
 	public List<ManagementFeeVo> listByUnitAndPeriod(SqlSessionTemplate sst, ManagementFeeVo vo) {
-		
+
 		List<ManagementFeeVo> list = sst.selectList("ManagementFeeMapper.listByUnitForYear", vo);
 		System.out.println("조회된" + list);
 		return list;
@@ -22,10 +22,19 @@ public class ManagementFeeDao {
 	public List<ManagementFeeVo> list(SqlSessionTemplate sst) {
 		return sst.selectList("ManagementFeeMapper.list");
 	}
-	
+
 	//view
-//	public List name() {
-//		
-//	}
+	//	public List name() {
+	//		
+	//	}
+
+	//청구번호 조회
+	public String getBilingNo(SqlSessionTemplate sst, ManagementFeeVo feeVo) {
+		return sst.selectOne("ManagementFeeMapper.getBilingNo", feeVo);
+	}
+	//기존 청구내역에 추가 
+	public int updateFacilitiesFee(SqlSessionTemplate sst, ManagementFeeVo feeVo) {
+		return sst.update("ManagementFeeMapper.updateFacilitiesFee", feeVo);
+	}
 
 }
